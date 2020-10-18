c223ec57ce2909e98be161245709f68140a282a7
Cory <coryf65@gmail.com>
2020-10-18 17:19:30 -0500

    Update EnemyWaveManager.cs
    Changed to use a State like way of spawning enemies

--- TowerBuilder/Assets/Scripts/EnemyWaveManager.cs
+++ TowerBuilder/Assets/Scripts/EnemyWaveManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class EnemyWaveManager : MonoBehaviour
 {
+    // creating a State like design
+    private enum State
+    {
+        WaitingToSpawnNextWave,
+        SpawningWave,
+    }
+    [SerializeField] private Transform spawnPositionTransform;
+    [SerializeField] private State currentState;
     private float nextWaveSpawnTimer;
     private float enemySpawnTimer;
     private int enemiesToSpawnCount;
@@ -12,42 +20,53 @@ public class EnemyWaveManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // setting State
+        currentState = State.WaitingToSpawnNextWave;
+
         nextWaveSpawnTimer = 3f;
     }
 
     private void Update()
     {
-        nextWaveSpawnTimer -= Time.deltaTime;
-        if (nextWaveSpawnTimer < 0)
+        switch (currentState)
         {
-            SpawnWave();
-        }
+            case State.WaitingToSpawnNextWave:
+                nextWaveSpawnTimer -= Time.deltaTime;
+                if (nextWaveSpawnTimer < 0)
+                {
+                    SpawnWave();
+                }
+                break;
+            case State.SpawningWave:
+                // check how many enemies we have left to spawn
+                if (enemiesToSpawnCount > 0)
+                {
+                    enemySpawnTimer -= Time.deltaTime;
+                    if (enemySpawnTimer < 0)
+                    {
+                        // Reset the timer
+                        enemySpawnTimer = Random.Range(0, .2f);
+                        // Spawn an enemy
+                        Enemy.Create(spawnPosition + UtilsClass.GetRandomDirection() * Random.Range(0f, 10f));
+                    }
+                    // we spawned one change counter
+                    enemiesToSpawnCount--;
 
-        // check how many enemies we have left to spawn
-        if (enemiesToSpawnCount > 0)
-        {
-            enemySpawnTimer -= Time.deltaTime;
-            if (enemySpawnTimer < 0)
-            {
-                // Reset the timer
-                enemySpawnTimer = Random.Range(0, .2f);
-                // Spawn an enemy
-                Enemy.Create(spawnPosition + UtilsClass.GetRandomDirection() * Random.Range(0f, 10f));
-            }
-            // we spawned one change counter
-            enemiesToSpawnCount--;
-        }
+                    if (enemiesToSpawnCount <= 0)
+                    {
+                        currentState = State.WaitingToSpawnNextWave;
+                    }
+                }
+                break;
+        }               
     }
 
     private void SpawnWave()
     {
-        spawnPosition = new Vector3(40, 0);
-
-        for (int count = 0; count < 10; count++)
-        {
-            nextWaveSpawnTimer = 10f; // 10 seconds
-            enemiesToSpawnCount = 10; // 10 enemies total
-        }
+        spawnPosition = spawnPositionTransform.position;
+        nextWaveSpawnTimer = 10f; // 10 seconds
+        enemiesToSpawnCount = 10; // 10 enemies total
+        currentState = State.SpawningWave;
     }
 
 }
