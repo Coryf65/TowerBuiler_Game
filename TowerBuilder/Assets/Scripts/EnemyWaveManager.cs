2d7fe5b7dedcff2f9b315b2119f02383ea9ae03b
Cory <coryf65@gmail.com>
2020-10-18 17:06:19 -0500

    Update EnemyWaveManager.cs
    Creating a Wave based enemy Spawner

--- TowerBuilder/Assets/Scripts/EnemyWaveManager.cs
+++ TowerBuilder/Assets/Scripts/EnemyWaveManager.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class EnemyWaveManager : MonoBehaviour
 {
+    private float nextWaveSpawnTimer;
+    private float enemySpawnTimer;
+    private int enemiesToSpawnCount;
+    Vector3 spawnPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        SpawnWave();
+        nextWaveSpawnTimer = 3f;
+    }
+
+    private void Update()
+    {
+        nextWaveSpawnTimer -= Time.deltaTime;
+        if (nextWaveSpawnTimer < 0)
+        {
+            SpawnWave();
+        }
+
+        // check how many enemies we have left to spawn
+        if (enemiesToSpawnCount > 0)
+        {
+            enemySpawnTimer -= Time.deltaTime;
+            if (enemySpawnTimer < 0)
+            {
+                // Reset the timer
+                enemySpawnTimer = Random.Range(0, .2f);
+                // Spawn an enemy
+                Enemy.Create(spawnPosition + UtilsClass.GetRandomDirection() * Random.Range(0f, 10f));
+            }
+            // we spawned one change counter
+            enemiesToSpawnCount--;
+        }
     }
 
     private void SpawnWave()
     {
-        Vector3 spawnPosition = new Vector3(20, 0);
+        spawnPosition = new Vector3(40, 0);
 
         for (int count = 0; count < 10; count++)
         {
-            // adding some randomness to the generator
-            Enemy.Create(spawnPosition + UtilsClass.GetRandomDirection() * Random.Range(0f, 10f));
+            nextWaveSpawnTimer = 10f; // 10 seconds
+            enemiesToSpawnCount = 10; // 10 enemies total
         }
     }
 
