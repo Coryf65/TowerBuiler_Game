15487cf712d986c755dc2172fd21a900dbcfcf7c
Cory <coryf65@gmail.com>
2020-10-18 16:50:04 -0500

    WaveSpawner Created
    Created a simple wave spawner for the enemies

---
+++ TowerBuilder/Assets/Scripts/EnemyWaveManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaveManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnWave();
+    }
+
+    private void SpawnWave()
+    {
+        Vector3 spawnPosition = new Vector3(20, 0);
+
+        for (int count = 0; count < 10; count++)
+        {
+            // adding some randomness to the generator
+            Enemy.Create(spawnPosition + UtilsClass.GetRandomDirection() * Random.Range(0f, 10f));
+        }
+    }
+
+}
