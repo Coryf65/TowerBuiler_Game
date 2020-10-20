d0a34a5b69b13741f1bf38c78871c0f6c6419316
Cory <coryf65@gmail.com>
2020-10-19 22:33:30 -0500

    New Demolish Button
    We can now demolish a placed building and recover 60% of the resource cost

--- TowerBuilder/Assets/Scripts/Building.cs
+++ TowerBuilder/Assets/Scripts/Building.cs
@@ -6,6 +6,13 @@ public class Building : MonoBehaviour
 {
     HealthSystem healthSystem;
     BuildingTypeSO buildingType;
+    private Transform buildingDemolishButton;
+
+    private void Awake()
+    {
+        buildingDemolishButton = transform.Find("pf_BuildingDemolishBtn");
+        HideBuildingDemolishBtn();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -28,5 +35,37 @@ public class Building : MonoBehaviour
     private void HealthSystem_OnDestroyed(object sender, System.EventArgs e)
     {
         Destroy(gameObject);
+    }
+
+    /// <summary>
+    ///  Handling the Demolish button using our collider
+    /// </summary>
+    private void OnMouseEnter()
+    {
+        ShowBuildingDemolishBtn();
+    }
+
+    /// <summary>
+    /// Handling the Demolish button using our collider
+    /// </summary>
+    private void OnMouseExit()
+    {
+        HideBuildingDemolishBtn();
+    }
+
+    private void ShowBuildingDemolishBtn()
+    {
+        if (buildingDemolishButton != null)
+        {
+            buildingDemolishButton.gameObject.SetActive(true);
+        }
+    }
+
+    private void HideBuildingDemolishBtn()
+    {
+        if (buildingDemolishButton != null)
+        {
+            buildingDemolishButton.gameObject.SetActive(false);
+        }
     }
 }
---
+++ TowerBuilder/Assets/Scripts/BuildingDemolishBtn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingDemolishBtn : MonoBehaviour
+{
+
+    [SerializeField] private Building building;
+
+    private void Awake()
+    {
+        // adding a destroy to our button
+        transform.Find("Button").GetComponent<Button>().onClick.AddListener(() => {
+            // when destroying we can get some resources back
+            BuildingTypeSO buildingType = building.GetComponent<BuildingTypeHolder>().buildingType;
+            foreach (ResourceAmount resourceAmount in buildingType.ConstructionResourceCost)
+            {
+                ResourceManager.TheResourceManager.AddResource(resourceAmount.resourceType, Mathf.FloorToInt(resourceAmount.amount * .6f)); // recoup 60%
+            }
+
+            // Remove the building
+            Destroy(building.gameObject);
+        });
+    }
+}
